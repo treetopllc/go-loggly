e59f6662ba8948f37c0882a94e35ee4f7ff8c27a
Christian Mesh <christianmesh1@gmail.com>
2015-02-02 21:01:34 +0000

    Simplify Client and remove useless async queue

---
+++ client_test.go
@@ -0,0 +1,37 @@
+package loggly_test
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/treetopllc/go-loggly"
+)
+
+type Message struct {
+	id int
+}
+
+func (m Message) String() string {
+	return fmt.Sprintf(`
+{
+	"id": "%v",
+	"msg": "Message
+strings: %v"
+}`, m.id, strings.Repeat("TreeTop", m.id%4))
+
+}
+
+func TestClient(t *testing.T) {
+	c := loggly.NewClient("<your token here>", "noblehack", "go3-big")
+	count := 10
+	sleep := time.Millisecond * 200
+	start := time.Now()
+	for i := 0; i < count; i++ {
+		c.Send([]byte(Message{i}.String()))
+		time.Sleep(sleep)
+	}
+	end := time.Now()
+	fmt.Printf("Took %v per\n", (end.Sub(start))/time.Duration(count)-sleep)
+}
